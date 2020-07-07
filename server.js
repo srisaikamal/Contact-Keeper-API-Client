3968fa692ea962188154cf3b6cceb8a8fa242c60
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2020-07-07 20:30:43 +0530

    final

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ app.get("/", (req, res) => {
   console.log("Hi");
   res.json({ msg: "welcome to contact keeper API" });
 });
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
